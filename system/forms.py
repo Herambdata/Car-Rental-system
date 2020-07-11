9d0785aaf275439c786c2b9af1f326f89c4fcb8d
Herambdata <65069411+Herambdata@users.noreply.github.com>
2020-07-11 20:57:26 +0530

    Add files via upload

---
+++ system/forms.py
@@ -0,0 +1,40 @@
+from django import forms
+from .models import Car, Order, PrivateMsg,CarType
+
+class CarForm(forms.ModelForm):
+    class Meta:
+        model = Car
+        fields = [
+            "image",
+            "car_name",
+            "company_name",
+            "plate_number",
+            "num_of_seats",
+            "num_of_bed",
+            "cost_par_day",
+            "cost_per_week",
+            "weekend_cost",
+            "avaliable",
+            # "car_type",
+            "content",
+        ]
+        
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = [
+            "car_name",
+            "employee_name",
+            "cell_no",
+            "address",
+            "date",
+            "to",
+        ]
+class MessageForm(forms.ModelForm):
+    class Meta:
+        model = PrivateMsg
+        fields = [
+            "name",
+            "email",
+            "message",
+        ]
